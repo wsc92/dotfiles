85703803ed194889e996e20a3f2d9a73a0dbf4e4
wsc92 <wscraig92@gmail.com>
2024-05-09 12:56:07 -0400

    fixing services

--- install.sh
+++ install.sh
@@ -19,17 +19,19 @@ sleep 4
 sudo pacman -S base-devel sddm alacritty kitty unzip nmap qtile pyright nwg-look metasploit lua-language-server pavucontrol blueman wireshark-qt clang gcc gdb eza udiskie github-cli btop neovim picom redshift rofi zellij rofi ruby starship npm ripgrep fzf feh ranger discord firefox nitrogen nemo samba ghidra dunst flameshot xorg-xrandr lxqt-policykit pasystray obsidian nerd-fonts -y
 sleep 4
 
-yay -S archlinux-tweak-tool-git wget morgen burpsuite sddm-sugar-dark qtile-extras-git
+yay -S archlinux-tweak-tool-git snapd wget morgen burpsuite sddm-sugar-dark qtile-extras-git
 sleep 4
 
 #enable sddm
 sudo systemctl enable sddm &
+sudo systemctl enable --now snapd.socket &
 sleep 4
 
 source .install/cava.sh
 sleep 4
 
 source .install/editor.sh
+source .install/app.sh
 #make directories and install theme
 source .install/sddm.sh
 source .install/type.sh
