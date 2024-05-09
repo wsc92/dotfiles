0681009847fc21475d107e4d5aae24a8344254a1
wsc92 <wscraig92@gmail.com>
2024-05-09 11:02:37 -0400

    neovim packages

---
+++ .install/editor.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo pacman -S lazygit fd cargo julia php ruby vscode-json-languageserver luarocks shfmt stylua -y
+gem install neovim
+sudo npm install -g neovim
--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ sleep 4
 sudo pacman -S base-devel sddm alacritty kitty unzip nmap qtile pyright nwg-look lua-language-server pavucontrol blueman wireshark-qt clang gcc gdb eza udiskie github-cli btop neovim picom redshift rofi zellij rofi ruby starship npm ripgrep fzf feh ranger discord firefox nitrogen nemo flameshot xorg-xrandr lxqt-policykit pasystray obsidian nerd-fonts -y
 sleep 4
 
-yay -S archlinux-tweak-tool-git morgen burpsuite drawio sddm-sugar-dark qtile-extras-git
+yay -S archlinux-tweak-tool-git wget morgen burpsuite drawio sddm-sugar-dark qtile-extras-git
 sleep 4
 
 #enable sddm
@@ -29,6 +29,7 @@ sleep 4
 source .install/cava.sh
 sleep 4
 
+source .install/editor.sh
 #make directories and install theme
 source .install/sddm.sh
 source .install/type.sh
