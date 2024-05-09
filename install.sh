b304dde6d2f0a2347eaf316401a565511fbf96b0
wsc92 <wscraig92@gmail.com>
2024-05-09 11:28:31 -0400

    added package

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ sudo pacman -Syu
 sleep 4
 
 #install packages
-sudo pacman -S base-devel sddm alacritty kitty unzip nmap qtile pyright nwg-look metasploit lua-language-server pavucontrol blueman wireshark-qt clang gcc gdb eza udiskie github-cli btop neovim picom redshift rofi zellij rofi ruby starship npm ripgrep fzf feh ranger discord firefox nitrogen nemo flameshot xorg-xrandr lxqt-policykit pasystray obsidian nerd-fonts -y
+sudo pacman -S base-devel sddm alacritty kitty unzip nmap qtile pyright nwg-look metasploit lua-language-server pavucontrol blueman wireshark-qt clang gcc gdb eza udiskie github-cli btop neovim picom redshift rofi zellij rofi ruby starship npm ripgrep fzf feh ranger discord firefox nitrogen nemo samba flameshot xorg-xrandr lxqt-policykit pasystray obsidian nerd-fonts -y
 sleep 4
 
 yay -S archlinux-tweak-tool-git wget morgen burpsuite sddm-sugar-dark qtile-extras-git
