528eee21b23a93f40bdcaa451b6ed000f90b4b86
wsc92 <wscraig92@gmail.com>
2024-05-15 17:25:34 -0400

    added required package

--- .install/packages.sh
+++ .install/packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo pacman -S base-devel sddm alacritty kitty unzip nmap alsa-utils qtile pyright nwg-look metasploit lua-language-server \
 	pavucontrol blueman wireshark-qt clang fish gcc gdb eza udiskie github-cli btop neovim picom redshift rofi zellij rofi ruby \
-	starship npm ripgrep fzf feh ranger discord firefox nitrogen nemo samba ghidra dunst flameshot xorg-xrandr lxqt-policykit \
+	starship npm ripgrep fzf feh slock ranger discord firefox nitrogen nemo samba ghidra dunst flameshot xorg-xrandr lxqt-policykit \
 	pasystray obsidian zathura nerd-fonts -y
 sleep 4
 
