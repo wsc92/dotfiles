751e17522c2119c6223385a688ca79c74148cbe6
wsc92 <wscraig92@gmail.com>
2024-05-17 18:20:43 -0400

    packages

--- .install/packages.sh
+++ .install/packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo pacman -S base-devel sddm alacritty kitty unzip nmap alsa-utils zoxide qtile pyright nwg-look metasploit lua-language-server \
-	pavucontrol blueman wireshark-qt clang fish gcc gdb eza udiskie github-cli btop neovim picom redshift rofi zellij rofi ruby \
+	pavucontrol blueman wireshark-qt clang fish gcc gdb noto-fonts-emoji eza udiskie github-cli btop neovim picom redshift rofi zellij rofi ruby \
 	starship npm ripgrep fzf feh slock vscode cbatticon ranger discord firefox nitrogen nemo samba ghidra dunst flameshot xorg-xrandr lxqt-policykit \
 	pasystray obsidian zathura nerd-fonts -y
 sleep 4
