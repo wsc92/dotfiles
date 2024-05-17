ae9ec7c681c088b45467a03cb5de6f6f47c4f684
wsc92 <wscraig92@gmail.com>
2024-05-17 16:57:34 -0400

    fix installer

--- install.sh
+++ install.sh
@@ -11,10 +11,6 @@ source .install/yay.sh
 
 sleep 4
 
-sudo pacman -Syu
-
-sleep 4
-
 #install packages
 source .install/packages.sh
 
@@ -28,11 +24,8 @@ sleep 4
 source .install/editor.sh
 #make directories and install theme
 source .install/sddm.sh
-source .install/type.sh
 cp -r ~/dotfiles/wallpapers ~
 source .install/file.sh
-# source .install/python.sh
-
 sleep 4
 
 source .install/reboot.sh
