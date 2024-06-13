3bcd629f24b6119072bf1c27a36125b3f787cf8a
cogent <wscraig92@yahoo.com>
2024-06-13 10:30:10 -0400

    menu update

--- configs/nvim/lua/plugins/ui.lua
+++ configs/nvim/lua/plugins/ui.lua
@@ -21,19 +21,9 @@ return {
   },
 
   --BufferLine
-  {
-    "akinsho/bufferline.nvim",
-    keys = {
-      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
-      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
-    },
-    opts = {
-      options = {
-        mode = "tabs",
-      },
-    },
-  },
-  --StatusLine
+  { "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons" },
+
+  --StatusLineNC
   {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -45,50 +35,65 @@ return {
         -- section_separators = { left = "", right = "" },
         -- section_separators = { left = "", right = "" },
         section_separators = { left = "", right = "" },
-        disabled_filetypes = {
-          statusline = {},
-          winbar = {},
-        },
-        ignore_focus = {},
-        always_divide_middle = true,
-        globalstatus = false,
-        refresh = {
-          statusline = 1000,
-          tabline = 1000,
-          winbar = 1000,
-        },
       },
-      sections = {},
-      tabline = {},
-      winbar = {},
-      inactive_winbar = {},
-      extensions = {},
     },
   },
-
   -- Filename
   {
     "b0o/incline.nvim",
     event = "BufReadPre",
     priority = 1200,
     config = function()
+      local devicons = require("nvim-web-devicons")
       require("incline").setup({
-        highlight = {
-          groups = {},
-        },
-        window = { margin = { vertical = 0, horizontal = 1 } },
-        hide = {
-          cursorline = true,
-        },
         render = function(props)
           local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-          if vim.bo[props.buf].modified then
-            filename = "[+]" .. filename
+          if filename == "" then
+            filename = "[No Name]"
+          end
+          local ft_icon, ft_color = devicons.get_icon_color(filename)
+
+          local function get_git_diff()
+            local icons = { removed = "", changed = "", added = "" }
+            local signs = vim.b[props.buf].gitsigns_status_dict
+            local labels = {}
+            if signs == nil then
+              return labels
+            end
+            for name, icon in pairs(icons) do
+              if tonumber(signs[name]) and signs[name] > 0 then
+                table.insert(labels, { icon .. signs[name] .. " ", group = "Diff" .. name })
+              end
+            end
+            if #labels > 0 then
+              table.insert(labels, { "┊ " })
+            end
+            return labels
           end
 
-          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+          local function get_diagnostic_label()
+            local icons = { error = "", warn = "", info = "", hint = "󰠗" }
+            local label = {}
 
-          return { { icon, guifg = color }, { " " }, { filename } }
+            for severity, icon in pairs(icons) do
+              local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
+              if n > 0 then
+                table.insert(label, { icon .. n .. " ", group = "DiagnosticSign" .. severity })
+              end
+            end
+            if #label > 0 then
+              table.insert(label, { "┊ " })
+            end
+            return label
+          end
+
+          return {
+            { get_diagnostic_label() },
+            { get_git_diff() },
+            { (ft_icon or "") .. " ", guifg = ft_color, guibg = "none" },
+            { filename .. " ", gui = vim.bo[props.buf].modified and "bold,italic" or "bold" },
+            { "┊  " .. vim.api.nvim_win_get_number(props.win), group = "DevIconWindows" },
+          }
         end,
       })
     end,
