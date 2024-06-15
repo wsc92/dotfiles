8b6cbb14479a4d1527e87b60bf31214dd2feb32e
cogent <wscraig92@yahoo.com>
2024-06-15 10:28:31 -0400

    update packages

--- .install/packages.sh
+++ .install/packages.sh
@@ -2,7 +2,7 @@
 sudo pacman -S base-devel sddm alacritty kitty unzip nmap alsa-utils zoxide qtile pyright nwg-look metasploit lua-language-server \
 	pavucontrol blueman wireshark-qt clang fish gcc gdb noto-fonts-emoji eza udiskie github-cli btop neovim picom redshift rofi zellij rofi ruby \
 	starship npm ripgrep fzf feh slock vscode cbatticon ranger discord firefox nitrogen nemo samba ghidra dunst flameshot xorg-xrandr lxqt-policykit \
-	pasystray obsidian zathura nerd-fonts -y
+	pasystray cloc obsidian zathura nerd-fonts -y
 sleep 4
 
 yay -S archlinux-tweak-tool-git wget morgen burpsuite ida-free zenmap sddm-sugar-dark qtile-extras-git
