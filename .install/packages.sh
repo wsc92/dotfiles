841fe7aa3dfc72865166518f609bb5051d5afc51
cogent <wscraig92@yahoo.com>
2024-07-08 12:02:10 -0400

    added packages

--- .install/packages.sh
+++ .install/packages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo pacman -S base-devel sddm alacritty kitty unzip nmap alsa-utils zoxide qtile pyright nwg-look metasploit lua-language-server \
-	pavucontrol blueman wireshark-qt clang fish gcc gdb noto-fonts-emoji eza udiskie github-cli btop neovim picom redshift rofi tmux rofi ruby \
-	starship npm ripgrep fzf feh slock vscode cbatticon ranger discord firefox nitrogen nemo samba ghidra dunst flameshot xorg-xrandr lxqt-policykit \
-	pasystray cloc obsidian zathura nerd-fonts -y
+    pavucontrol blueman wireshark-qt clang fish gcc gdb noto-fonts-emoji eza udiskie github-cli btop neovim picom redshift rofi tmux rofi ruby \
+    starship npm ripgrep fzf feh slock vscode cbatticon ranger discord firefox nitrogen nemo samba ghidra dunst flameshot xorg-xrandr lxqt-policykit \
+    pasystray cmus gedit steam cloc obsidian zathura nerd-fonts -y
 sleep 4
 
 yay -S archlinux-tweak-tool-git wget morgen burpsuite ida-free zenmap sddm-sugar-dark qtile-extras-git
